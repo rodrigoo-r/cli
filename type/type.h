8f272a5b62fea661df7f8b5a513ebaa0d817a8d3
rodrigoo-r <2nw53j32643y@gmail.com>
2025-05-27 15:04:33 -0500

    Add C++ extern "C" linkage to type.h for compatibility

--- type/type.h
+++ type/type.h
@@ -19,6 +19,12 @@
 #ifndef FLUENT_LIBC_CLI_TYPE_H
 #define FLUENT_LIBC_CLI_TYPE_H
 
+// ============= FLUENT LIB C++ =============
+#if defined(__cplusplus)
+extern "C"
+}
+#endif
+
 /**
  * @enum cli_type_t
  * @brief Enumerates the types of command-line arguments supported.
@@ -38,5 +44,10 @@ typedef enum
     CLI_TYPE_FLOAT,    /**< Float flag/command */
     CLI_TYPE_ARRAY,    /**< Array flag/command */
 } cli_type_t;
+
+// ============= FLUENT LIB C++ =============
+#if defined(__cplusplus)
+}
+#endif
 
 #endif //FLUENT_LIBC_CLI_TYPE_H
