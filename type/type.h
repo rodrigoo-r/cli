a62b334c3d9270a0520a376dcf96f8655f6a61f9
rodrigoo-r <2nw53j32643y@gmail.com>
2025-05-29 07:37:44 -0500

    Update license information in type.h for clarity and consistency

--- type/type.h
+++ type/type.h
@@ -1,15 +1,11 @@
 /*
-    The Fluent Programming Language
-    -----------------------------------------------------
-    This code is released under the GNU GPL v3 license.
-    For more information, please visit:
-    https://www.gnu.org/licenses/gpl-3.0.html
-    -----------------------------------------------------
-    Copyright (c) 2025 Rodrigo R. & All Fluent Contributors
-    This program comes with ABSOLUTELY NO WARRANTY.
-    For details type `fluent l`. This is free software,
-    and you are welcome to redistribute it under certain
-    conditions; type `fluent l -f` for details.
+ * This code is distributed under the terms of the GNU General Public License.
+ * For more information, please refer to the LICENSE file in the root directory.
+ * -------------------------------------------------
+ * Copyright (C) 2025 Rodrigo R.
+ * This program comes with ABSOLUTELY NO WARRANTY; for details type show w'.
+ * This is free software, and you are welcome to redistribute it
+ * under certain conditions; type show c' for details.
 */
 
 //
